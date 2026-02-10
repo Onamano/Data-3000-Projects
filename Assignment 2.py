31f5c3182677c661c58b96d9fd7fd03d6343f3e0
Mike <129209632+Onamano@users.noreply.github.com>
2026-02-09 23:48:09 -0700

    Adding instructions as comments. None of this Java shit.

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,2 +1,28 @@
-# Read employee data from included file - ensure error handling and try/except
+# Read employee data from included file - ensure error handling and try/except - and populates an array to create 1000 employee objects
 # Needs to sort using an algorithm, rather than the sort() function
+
+
+# Create a calcHourlySalary() method which calculates the hourly salary based on the attributes of each employee
+
+
+#Create a utility class named SelectionSort with a static method which performs the selection sort on generic data types. Objects must be implemented in the Comparable interface and compared based on hourly salary
+
+
+# Create a second utility class named QuickSort with a static method. Thos should perform the quicksort algorithm on an array of generic data types. Implement the comparable interface to be compared by name
+
+
+# Measure the running time for each sorting algorithm using System.current.TimeMillis() (or whatever the hell it is in Python) and comepare the performance and print the running time of each to the console
+
+
+#  After sorting the array of employee objects, write the sorted array to two files named sortedemployeeBySalary.csv and sortedemployeeByName.csv using the same format as the input file
+#  Example: 1. John Smith,40,15.50,0.05,0.10,1000.00
+
+
+# Implement a thurd utility class named BinarySearch with a static method. This performs a recursive binary search on a sorted array of generic data type. 
+# Implement the comparable to compare by name and return the index position of the first occurance of the employees name if it appears multiple times. If name is not found, return -1
+
+
+# Print results of the binary search to the console, indicating whether the employees name was found
+
+
+# Prompt the user to enter a name to easrch for. Perform the binary search on the sorted array and print the result to the console
